94c663ea5f304c2a98faa7d4ae10f7bc24c5ae5e
Siva Kandula <sriramsmiling2012@gmail.com>
2023-07-25 09:40:25 +0530

    Formatting Corrected with autopep8

--- mathprobs.py
+++ mathprobs.py
@@ -1,9 +1,10 @@
 from random import shuffle
 
+
 def prime_checker():
     num = int(input("Enter a number: \n "))
     prime_flag = False
-    if num > 1: 
+    if num > 1:
         for i in range(2, int(num**0.5)+1):
             if (num % i) == 0:
                 # if factor is found, set flag to True
@@ -18,92 +19,102 @@ def prime_checker():
     else:
         print(num, "is not a prime number")
 
+
 def has33(list):
-    index=0
-    checker=False
+    index = 0
+    checker = False
     for item in list:
-        if list[index]==3 and index < (len(list)-1): 
-            if list[index+1]==3:
+        if list[index] == 3 and index < (len(list)-1):
+            if list[index+1] == 3:
                 print('True, it contains 3,3 successively')
-                checker=True
+                checker = True
                 break
             else:
                 pass
-        else :
-             pass
-        index+=1
-    if(not checker):
+        else:
+            pass
+        index += 1
+    if (not checker):
         print("False, it doesn't contains 3,3 successively")
 
+
 def spy_game_long(list):
-    final_list=[]
-    checker=False
-    index=0
+    final_list = []
+    checker = False
+    index = 0
     for item in list:
-        if item==0 or item==7:
+        if item == 0 or item == 7:
             final_list.append(item)
-        else :
+        else:
             pass
     for item2 in final_list:
-        if final_list[index]==0 and index < (len(final_list)-2):
-            if final_list[index+1]==0:
-                if final_list[index+2]==7:
+        if final_list[index] == 0 and index < (len(final_list)-2):
+            if final_list[index+1] == 0:
+                if final_list[index+2] == 7:
                     print('I am James Bond 007.Friends call me Siva')
-                    checker=True
+                    checker = True
                     break
-                else :
+                else:
                     pass
-            else :
+            else:
                 pass
-        else :
+        else:
             pass
-        index+=1
+        index += 1
     if (not checker):
         print('This is not 007 list')
 
+
 def spy_game_short(list):
-    temp_list=[0,0,7,'x']
+    temp_list = [0, 0, 7, 'x']
     for num in list:
-        if num==temp_list[0]:
+        if num == temp_list[0]:
             temp_list.pop(0)
-    if len(temp_list)==1:
+    if len(temp_list) == 1:
         print('I am James Bond 007.Friends call me Siva')
     else:
         print('This is not 007 list')
+
 
 '''Write a Python function that takes a list and returns a new list with unique elements of the first list.
 
 Sample List : [1,1,1,1,2,2,3,3,3,3,4,5]
 Unique List : [1, 2, 3, 4, 5]
 '''
+
+
 def unique_list(list):
-       print([item for item in set(list)])
+    print([item for item in set(list)])
+
 
 def fibonoccigenerator():
     '''Generator that generates the fibonocci series upto number n'''
-    number=int(input('Enter the number to generate fibonocci upto : '))
+    number = int(input('Enter the number to generate fibonocci upto : '))
+
     def fibgen():
-        a=1
-        b=1
+        a = 1
+        b = 1
         for item in range(number):
-            while(a<=number):
+            while (a <= number):
                 yield a
-                a,b=b,a+b
+                a, b = b, a+b
     for item in fibgen():
         print(item)
 
+
 def randonnumgenerators():
     '''Geneartor for n randon numbers in range low and high'''
-    low=int(input('Enter lower limit to generate random numbers : '))
-    high= int(input('Enter higher limit to generate random numbers : '))
-    n= int(input('Enter how many random numbers you want : '))
-    mylist=[num for num in range(low,high)]
+    low = int(input('Enter lower limit to generate random numbers : '))
+    high = int(input('Enter higher limit to generate random numbers : '))
+    n = int(input('Enter how many random numbers you want : '))
+    mylist = [num for num in range(low, high)]
     shuffle(mylist)
+
     def randomgen():
-        counter=0
+        counter = 0
         for number in mylist:
-            if counter<n:
+            if counter < n:
                 yield number
-                counter+=1
+                counter += 1
     for number in randomgen():
-                print(number)
+        print(number)
--- stringprobs.py
+++ stringprobs.py
@@ -1,31 +1,36 @@
 import string
 
+
 def string_reversal():
-        input_string=input('Enter the input String \n ')
-        print(input_string[::-1])  
+    input_string = input('Enter the input String \n ')
+    print(input_string[::-1])
+
 
 def count_string_chars():
-        mydict={}
-        input_string=input('Enter the input String \n ')
-        for item in input_string:
-                if item not in mydict:
-                        mydict[item]=1
-                else :
-                        mydict[item]=mydict.get(item)+1
-        print(mydict)
+    mydict = {}
+    input_string = input('Enter the input String \n ')
+    for item in input_string:
+        if item not in mydict:
+            mydict[item] = 1
+        else:
+            mydict[item] = mydict.get(item)+1
+    print(mydict)
+
 
 def sentence_reversed():
-    sentence=input("Enter any sentence \n ")
-    mylist=sentence.split()
-    output=mylist[::-1]
+    sentence = input("Enter any sentence \n ")
+    mylist = sentence.split()
+    output = mylist[::-1]
     print(' '.join(output))
 
+
 def paper_doll():
-        input_string=input('Enter any String \n ')
-        output=''
-        for item in input_string:
-              output=output+(item*3)
-        print(output)
+    input_string = input('Enter any String \n ')
+    output = ''
+    for item in input_string:
+        output = output+(item*3)
+    print(output)
+
 
 '''Write a Python function that accepts a string and calculates the number of upper case letters and lower case letters.
 
@@ -35,64 +40,75 @@ No. of Upper case characters : 4
 No. of Lower case Characters : 33
 
 '''
+
+
 def upper_lower_count():
-       sentence=input('Enter the sentence string \n ')
-       upper_counter=0
-       lower_counter=0
-       for item in sentence:
-              if item.isupper():
-                     upper_counter+=1
-              elif item.islower():
-                     lower_counter+=1       
-       print(f"No of upper case letters is {upper_counter}")
-       print(f"No of lower case letters is {lower_counter}")
+    sentence = input('Enter the sentence string \n ')
+    upper_counter = 0
+    lower_counter = 0
+    for item in sentence:
+        if item.isupper():
+            upper_counter += 1
+        elif item.islower():
+            lower_counter += 1
+    print(f"No of upper case letters is {upper_counter}")
+    print(f"No of lower case letters is {lower_counter}")
+
 
 '''
 Write a Python function that checks whether a word or phrase is palindrome or not.
 '''
+
+
 def word_pallindrome():
-       word=input('Enter the word  \n ')
-       if word==word[::-1]:
-              print(f'The word {word} is a pallindrome')
-       else :
-              print(f'The word {word} is not a pallindrome')
+    word = input('Enter the word  \n ')
+    if word == word[::-1]:
+        print(f'The word {word} is a pallindrome')
+    else:
+        print(f'The word {word} is not a pallindrome')
+
 
 '''
 Write a Python function to check whether a string is pangram or not. (Assume the string passed in does not have any punctuation)
 '''
 
+
 def ispangram(alphabet=string.ascii_lowercase):
-    alphaset=set(alphabet)
-    sentence=input('Enter the sentence \n ')
-    myset=set(sentence.replace(' ','').lower())
-    if len(myset) !=26:
-        print(f'This sentence is not a pangram, the unique letter count is {len(myset)}')
-    elif len(myset)==26 :
-        if myset==alphaset:
+    alphaset = set(alphabet)
+    sentence = input('Enter the sentence \n ')
+    myset = set(sentence.replace(' ', '').lower())
+    if len(myset) != 26:
+        print(
+            f'This sentence is not a pangram, the unique letter count is {len(myset)}')
+    elif len(myset) == 26:
+        if myset == alphaset:
             print('Hurray, this is pangram')
-        else :
-             print('This is not a pangram, one or more characters might not be alphabets')
+        else:
+            print('This is not a pangram, one or more characters might not be alphabets')
+
 
 '''Longest substring without repeating characters'''
 
+
 def longestSubString():
-        giveninput=input('Enter the string to find longest substring in it without repeated characters')
-        tempinput=giveninput
-        unique_list=[]
-        substring=''
-        for char in giveninput:
-         for char in tempinput:
-                if char not in substring:
-                 substring=substring+str(char)
-                else :
-                 unique_list.append(substring)
-                 substring=''
-                 tempinput=tempinput[1:]
-                 break
-        index=0
-        pos=0
-        for item in unique_list:
-          if len(item)>index:
-                index=len(item)
-                pos=unique_list.index(item)
-        print(unique_list[pos])
+    giveninput = input(
+        'Enter the string to find longest substring in it without repeated characters')
+    tempinput = giveninput
+    unique_list = []
+    substring = ''
+    for char in giveninput:
+        for char in tempinput:
+            if char not in substring:
+                substring = substring+str(char)
+            else:
+                unique_list.append(substring)
+                substring = ''
+                tempinput = tempinput[1:]
+                break
+    index = 0
+    pos = 0
+    for item in unique_list:
+        if len(item) > index:
+            index = len(item)
+            pos = unique_list.index(item)
+    print(unique_list[pos])
